61b168741c30e5fc361e6ad00823dcce37f4d887
TSUKADA RAITO <hellow.friend.117@gmail.com>
2021-12-10 15:05:42 +0900

    完成版（神さん２回目提出済み）

--- todo/src/screens/Todo.js
+++ todo/src/screens/Todo.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-// import { Button } from "../components/Button";
+import { IconContext } from "../../node_modules/react-icons";
 import { FaTrashAlt } from "../../node_modules/react-icons/fa";
-import { FaPlus } from "../../node_modules/react-icons/fa";
+import { FaPlusCircle } from "../../node_modules/react-icons/fa";
 
 export const Todo = () => {
   // 既存のタスクの代入
@@ -33,47 +33,85 @@ export const Todo = () => {
 
   const handleRemoveTask = (index) => {
     const newTodos = [...todos];
+    // spliceメソッドはindexから１個newTodosから取り除く
     newTodos.splice(index, 1);
     setTodo(newTodos);
   };
 
   return (
-    <div>
-      <div>
-        <form style={style.formStyle}>
-          {" "}
-          <input
-            value={task}
-            placeholder="テキストを入力･･･"
-            onChange={handleNewTask}
-          />
-          <span onClick={handleSubmit}>
-            <FaPlus />
-          </span>
-        </form>
-        <ul>
-          {todos.map((todo, index) => (
-            <li key={index} style={style.todoStyle}>
-              {todo.task}
-              {"   "}
-              <span onClick={() => handleRemoveTask(index)}>
+    <div style={style.total}>
+      <form style={style.form}>
+        <input
+          value={task}
+          placeholder="テキストを入力･･･"
+          onChange={handleNewTask}
+          style={style.input}
+        />
+        <span onClick={handleSubmit}>
+          <IconContext.Provider value={{ color: "#1760a0", size: "1.3em" }}>
+            <FaPlusCircle />
+          </IconContext.Provider>
+        </span>
+      </form>
+      <p style={style.line}></p>
+      <ul style={style.lists}>
+        {todos.map((todo, index) => (
+          <li key={index} style={style.todos}>
+            {todo.task}
+            <span onClick={() => handleRemoveTask(index)} style={style.trash}>
+              <IconContext.Provider value={{ color: "grey" }}>
                 <FaTrashAlt />
-              </span>
-            </li>
-          ))}
-        </ul>
-      </div>
+              </IconContext.Provider>
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
 const style = {
-  formStyle: {
-    textAlign: "center",
+  total: {
+    marginTop: "40px",
+    height: "800px",
+    width: "600px",
+    borderRadius: "10px",
+    marginLeft: "10px",
+    border: "4px solid gray",
   },
-  todoStyle: {
+  form: {
     textAlign: "center",
+    marginTop: "30px",
+    marginBottom: "30px",
+  },
+  input: {
+    width: "400px",
+    border: "none",
+    outline: "none",
+    borderBottom: "2px solid grey",
+  },
+  line: {
+    borderBottom: "2px solid grey",
+  },
+  lists: {
+    // backgroundColor: "tomato",
+    width: "560px",
+    // marginLeft: "10px",
+  },
+  todos: {
+    textAlign: "center",
+    width: "500px",
     listStyle: "none",
     fontSize: "20px",
+    border: "1px solid gray",
+    marginBottom: "20px",
+    borderRadius: "10px",
+    boxShadow: "2px 2px 4px gray",
+    display: "grid",
+    gridTemplateColumns: "4fr 1fr",
+  },
+  trash: {
+    textAlign: "center",
+    marginTop: "5px",
   },
 };
